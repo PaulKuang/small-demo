4d87e7cbfb8764664d8e220a5301677cba91efe9
paulkuang <paulkuang99@gmail.com>
2021-05-02 21:29:42 -0700

    add context example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Counter from './Counter'
+import CounterHooks from './CounterHooks'
+
+export const ThemeContext = React.createContext()
 
 function App() {
   console.log("Render App")
+  const [theme, setTheme] = useState('red')
   return (
-    <Counter initialCount={1} />
+    <ThemeContext.Provider value={{backgroundColor: theme}}>
+      Counter
+      <Counter initialCount={1} />
+      Counter Hooks
+      <CounterHooks initialCount={5} />
+      <button onClick={()=>setTheme(prevTheme => {
+        return prevTheme === 'red' ? 'blue' : 'red'
+      })}>Toggle Theme</button>
+    </ThemeContext.Provider>
   )
 }
 
